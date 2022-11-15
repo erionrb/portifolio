25446ddc2352607f913c61bef3d834d628c1f2a8
erionrb <erionrb@gmail.com>
2022-11-15 08:06:39 -0300

    added about and experience tab

---
+++ components/About.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+export default function About({}: Props) {
+  return (
+    <div
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row 
+    max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        src="https://media-exp1.licdn.com/dms/image/C4E03AQFzX7Di24xXnw/profile-displayphoto-shrink_200_200/0/1624280442774?e=1673481600&v=beta&t=f7rcKq_QcCBnWePg6roJr9rItECEADH8zzPGEFo2X-g"
+        className=" -mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+        md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          background
+        </h4>
+        <p className="text-sm">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Why do we use it? It is a long
+          established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like). 
+        </p>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
+import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import WorkExperience from "../components/WorkExperience";
 
 export default function Home() {
   return (
     <div
-      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+      className="bg-[rgb(36,36,36)] text-white snap-y snap-mandatory
      overflow-scroll z-0"
     >
       <Head>
@@ -16,13 +18,19 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
